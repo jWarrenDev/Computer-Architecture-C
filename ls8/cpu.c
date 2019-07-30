f9a01e6834124b5dc88b6123093753b3e8266753
Jerrick Warren <Jerrick.Warren@gmail.com>
2019-07-30 16:08:55 -0700

    worked on requirements 1-4

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,6 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "cpu.h"
-
 #define DATA_LEN 6
+
+// Step 2
+// In cpu.c, add functions cpu_ram_read() and cpu_ram_write() that access the RAM inside the struct cpu.
+
+// write the RAM
+void writeRam(struct cpu *cpu, unsigned char value, unsigned char index)
+{
+  /* data */
+  cpu->ram[index] = value;
+
+};
+
+// read the RAM
+
+void readRam(struct cpu *cpu, unsigned char value, unsigned char index)
+{
+  /* data */
+  cpu->ram[value] = index;
+};
+
+
+
 
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
@@ -40,6 +63,11 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
   }
 }
 
+
+
+// Step 4
+// Implement the core of cpu_run()
+
 /**
  * Run the CPU
  */
@@ -50,18 +78,30 @@ void cpu_run(struct cpu *cpu)
   while (running) {
     // TODO
     // 1. Get the value of the current instruction (in address PC).
+
+
     // 2. Figure out how many operands this next instruction requires
+
     // 3. Get the appropriate value(s) of the operands following this instruction
+
     // 4. switch() over it to decide on a course of action.
+
     // 5. Do whatever the instruction should do according to the spec.
+
     // 6. Move the PC to the next instruction.
+
   }
 }
 
+// Step 3. 
 /**
  * Initialize a CPU struct
  */
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
+  cpu->PC;
+  cpu->ram;
+  cpu->registers;
+
 }
